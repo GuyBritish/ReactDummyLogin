fe52660657b56ababae7b21f7eda85da6a879552
Max <80323354+GuyBritish@users.noreply.github.com>
2021-10-08 03:20:49 -0500

    fix bug

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,20 +6,20 @@ import Button from "../UI/Button/Button";
 
 const emailReducer = (state, action) => {
 	if (action.type === "EMAIL_INPUT") {
-		return { value: action.val, isValid: action.val.includes("@") };
+		return { value: action.value, isValid: action.value.includes("@") };
 	}
 	if (action.type === "INPUT_BLUR") {
-		return { value: state.val, isValid: state.val.includes("@") };
+		return { value: state.value, isValid: state.value.includes("@") };
 	}
 	return { value: "", isValid: false };
 };
 
 const passwordReducer = (state, action) => {
 	if (action.type === "PASSWORD_INPUT") {
-		return { value: action.val, isValid: action.val.trim().length > 6 };
+		return { value: action.value, isValid: action.value.trim().length > 6 };
 	}
 	if (action.type === "INPUT_BLUR") {
-		return { value: state.val, isValid: action.val.trim().length > 6 };
+		return { value: state.value, isValid: state.value.trim().length > 6 };
 	}
 };
 
@@ -42,24 +42,24 @@ const Login = (props) => {
 	useEffect(() => {
 		const formCheckDelay = setTimeout(() => {
 			console.log("Checking form!");
-			setFormIsValid(emailIsValid && passwordIsValid);
-		}, 800);
+			setFormIsValid(emailState.isValid && passwordState.isValid);
+		}, 500);
 		return () => {
 			console.log("Clean up!");
 			clearTimeout(formCheckDelay);
 		};
-	}, [emailIsValid, passwordIsValid]);
+	}, [emailState, passwordState]);
 
 	const emailChangeHandler = (event) => {
-		dispatchEmail({ type: "EMAIL_INPUT", val: event.target.value });
+		dispatchEmail({ type: "EMAIL_INPUT", value: event.target.value });
 
-		//setFormIsValid(emailState.isValid && passwordState.isValid);
+		setFormIsValid(event.target.value.includes("@") && passwordState.isValid);
 	};
 
 	const passwordChangeHandler = (event) => {
-		dispatchPassword({ type: "PASSWORD_INPUT", val: event.target.value });
+		dispatchPassword({ type: "PASSWORD_INPUT", value: event.target.value });
 
-		//setFormIsValid(emailState.isValid && passwordState.isValid);
+		setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
 	};
 
 	const validateEmailHandler = () => {
