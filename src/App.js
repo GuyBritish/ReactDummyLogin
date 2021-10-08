0ed9b20dcc1c0ccf385209658de12b87d3e41f11
Max <80323354+GuyBritish@users.noreply.github.com>
2021-10-08 02:56:08 -0500

    add React context

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
 
+import AuthContext from "./components/context/auth-context";
+
 function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -27,13 +29,13 @@ function App() {
 	};
 
 	return (
-		<React.Fragment>
+		<AuthContext.Provider>
 			<MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
 			<main>
 				{!isLoggedIn && <Login onLogin={loginHandler} />}
 				{isLoggedIn && <Home onLogout={logoutHandler} />}
 			</main>
-		</React.Fragment>
+		</AuthContext.Provider>
 	);
 }
 
