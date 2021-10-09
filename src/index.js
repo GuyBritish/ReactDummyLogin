0543dee6788a6efcff466c840bfc0e1044f60c2a
Max <80323354+GuyBritish@users.noreply.github.com>
2021-10-09 03:28:57 -0500

    App useContext refractoring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { AuthContestProvider } from "./components/context/auth-context";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<AuthContestProvider>
+		<App />
+	</AuthContestProvider>,
+	document.getElementById("root")
+);
